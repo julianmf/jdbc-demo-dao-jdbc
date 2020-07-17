23bc1dec2297e538acb396d63bc42ed9c9cd862b
julianmf <figueredo.juliano@hotmail.com>
2020-07-17 02:04:17 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,9 +31,17 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: seller insert =====");
+		System.out.println("\n=== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id: " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller insert =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+				
+		
 	}
 }
